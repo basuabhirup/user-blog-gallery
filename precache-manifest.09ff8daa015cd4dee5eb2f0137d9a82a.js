e43e678ca8c22232fd5e4bd557ba71b2ab858c71
basuabhirup <basu.abhirup@yahoo.com>
2020-08-27 12:56:18 +0530

    Updates

---
+++ precache-manifest.09ff8daa015cd4dee5eb2f0137d9a82a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f646c95729f15c4dc0cc00e90e67f24b",
+    "url": "/user-blog-gallery/index.html"
+  },
+  {
+    "revision": "095ccbeef9f542b02590",
+    "url": "/user-blog-gallery/static/css/main.74036bfb.chunk.css"
+  },
+  {
+    "revision": "2fa15e94d5ec5e53b2ae",
+    "url": "/user-blog-gallery/static/js/2.c94003cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-blog-gallery/static/js/2.c94003cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "095ccbeef9f542b02590",
+    "url": "/user-blog-gallery/static/js/main.1031778c.chunk.js"
+  },
+  {
+    "revision": "b649ef518711794dc039",
+    "url": "/user-blog-gallery/static/js/runtime-main.fb3ec9b8.js"
+  }
+]);
